6ada3f096a8215dc5f291286b689199e4e884ded
b07502089 <a6140000@gmail.com>
2022-01-27 18:25:04 +0800

    fix deploy bug

---
+++ pages/posts.js
@@ -0,0 +1,16 @@
+import Layout from '../layout/layout.js'
+import { Flex, Heading, Box, useColorModeValue } from '@chakra-ui/react'
+
+export default function Posts() {
+  const normalFontColor = useColorModeValue('#000', '#fff')
+
+  return (
+    <Layout>
+      <Box bg={useColorModeValue('gray.100', 'black')} h='100vh' maxW="screen-md" mx="auto" overflow="visible" p="64px">
+        <Flex h='90vh' justifyContent="center" mb={4} grow="1" flexDirection="column" alignItems="center"> 
+            <Heading as="h1" size="4xl" fontWeight="bold" color={normalFontColor} mb={4}>Posts.</Heading>
+        </Flex>
+      </Box>
+    </Layout>
+  )
+}
