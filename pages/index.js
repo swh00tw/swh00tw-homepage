9b12a1b3472ba6eb720ad29ab431b0613725a17e
b07502089 <a6140000@gmail.com>
2022-01-27 18:12:24 +0800

    add dark mode toggle

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from '../layout/layout.js'
-import { Flex, Heading, Box } from '@chakra-ui/react'
+import { Flex, Heading, Box, useColorModeValue } from '@chakra-ui/react'
 
 export default function Home() {
+  const normalFontColor = useColorModeValue('#000', '#fff')
+
   return (
     <Layout>
-      <Box h='100vh' maxW="screen-md" mx="auto" overflow="visible" p="64px">
+      <Box bg={useColorModeValue('gray.100', 'black')} h='100vh' maxW="screen-md" mx="auto" overflow="visible" p="64px">
         <Flex h='90vh' justifyContent="center" mb={4} grow="1" flexDirection="column" alignItems="center"> 
-            <Heading as="h1" size="4xl" fontWeight="bold" mb={4}>Hello, I am Frank.</Heading>
+            <Heading as="h1" size="4xl" fontWeight="bold" color={normalFontColor} mb={4}>Hello, I am Frank.</Heading>
         </Flex>
       </Box>
     </Layout>
