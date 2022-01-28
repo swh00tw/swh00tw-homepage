3b1e8c01ff092b93ccd58e624cf98b25ae0db867
b07502089 <a6140000@gmail.com>
2022-01-28 20:52:52 +0800

    move layout wrapper to _app.js

--- pages/projects.js
+++ pages/projects.js
@@ -5,10 +5,8 @@ export default function Projects() {
   const normalFontColor = useColorModeValue('#000', '#fff')
 
   return (
-    <Layout>
-        <Flex h='90vh' justifyContent="center" grow="1" flexDirection="column" alignItems="center"> 
-            <Heading as="h1" size="4xl" fontWeight="bold" color={normalFontColor} mb={4}>Projects.</Heading>
-        </Flex>
-    </Layout>
+      <Flex h='90vh' justifyContent="center" grow="1" flexDirection="column" alignItems="center"> 
+          <Heading as="h1" size="4xl" fontWeight="bold" color={normalFontColor} mb={4}>Projects.</Heading>
+      </Flex>
   )
 }
