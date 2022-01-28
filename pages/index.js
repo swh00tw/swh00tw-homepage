e26e8f95d0c8f3520bcf703e0eb20ccb1a02e9be
b07502089 <a6140000@gmail.com>
2022-01-28 20:52:52 +0800

    move layout wrapper to _app.js

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,8 @@ export default function Home(props) {
   const normalFontColor = useColorModeValue('#000', '#fff')
 
   return (
-    <Layout>
-        <Flex h='90vh' justifyContent="center" grow="1" flexDirection="column" alignItems="center"> 
-            <Heading as="h1" size="4xl" fontWeight="bold" color={normalFontColor} mb={4}>Hello, I am Frank.</Heading>
-        </Flex>
-    </Layout>
+      <Flex h='90vh' justifyContent="center" grow="1" flexDirection="column" alignItems="center"> 
+          <Heading as="h1" size="4xl" fontWeight="bold" color={normalFontColor} mb={4}>Hello, I am Frank.</Heading>
+      </Flex>
   )
 }
--- pages/posts.js
+++ pages/posts.js
@@ -5,10 +5,8 @@ export default function Posts() {
   const normalFontColor = useColorModeValue('#000', '#fff')
 
   return (
-    <Layout>
-        <Flex h='90vh' justifyContent="center" grow="1" flexDirection="column" alignItems="center"> 
-            <Heading as="h1" size="4xl" fontWeight="bold" color={normalFontColor} mb={4}>Posts.</Heading>
-        </Flex>
-    </Layout>
+      <Flex h='90vh' justifyContent="center" grow="1" flexDirection="column" alignItems="center"> 
+          <Heading as="h1" size="4xl" fontWeight="bold" color={normalFontColor} mb={4}>Posts.</Heading>
+      </Flex>
   )
 }
