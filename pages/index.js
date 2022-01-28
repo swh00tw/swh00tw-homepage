a887a3b0b0b15aee02396221ee02c95acc569139
b07502089 <a6140000@gmail.com>
2022-01-28 21:06:46 +0800

    add page motion container & refactor layout code

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import Layout from '../layout/layout.js'
+import PageMotionContainer from '../components/PageMotionContainer'
 import { Flex, Heading, Box, useColorModeValue } from '@chakra-ui/react'
 
 export default function Home(props) {
   const normalFontColor = useColorModeValue('#000', '#fff')
 
   return (
+    <PageMotionContainer>
       <Flex h='90vh' justifyContent="center" grow="1" flexDirection="column" alignItems="center"> 
           <Heading as="h1" size="4xl" fontWeight="bold" color={normalFontColor} mb={4}>Hello, I am Frank.</Heading>
       </Flex>
+    </PageMotionContainer>
   )
 }
