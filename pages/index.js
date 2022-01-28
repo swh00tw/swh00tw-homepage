9c821a387547a82c26b93fa2b784aac1d9373f82
b07502089 <a6140000@gmail.com>
2022-01-29 00:45:32 +0800

    add react state on clicking me

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,18 @@ import Image from 'next/image'
 import { motion } from 'framer-motion'
 import memojiStyle from '../styles/memoji.module.css'
 import { BsFillCaretRightFill, BsFillCaretDownFill } from "react-icons/bs";
+import {useState, useEffect} from 'react'
 
 export default function Home(props) {
+  const [isFistBumped, setIsFistBumped ] = useState(false);
+
   const normalFontColor = useColorModeValue('#000', '#fff')
   const themeColor = useColorModeValue('orange.600', 'purple.300')
   const bgColor = useColorModeValue('gray.100', 'black')
+
+  useEffect(() => {
+    console.log(isFistBumped);
+  },[isFistBumped])
 
   return (
     <PageMotionContainer>
@@ -19,15 +26,21 @@ export default function Home(props) {
             <Heading as="h2" size="lg" fontWeight="bold" color={normalFontColor} mb={1}>A web developer in Taiwan.</Heading>
             <Heading as="h2" size="lg" fontWeight="bold" color={normalFontColor}>Welcome to my website !</Heading>
             <Box as='text' fontFamily='mono' bg={useColorModeValue('gray.300','whiteAlpha.400')} borderRadius='lg' h={{base: '14', md: '7'}} py={1} px={5} mx='auto' mt={5} textAlign="center">
+              {isFistBumped? 
+              <HStack>
+              <Text>Nice 2 meet u ! </Text>
+              </HStack>
+              :
               <HStack>
               <Text>Click me to do a fist bump. </Text>
               <Flex display={{base: 'inline-block', lg: 'none'}}><BsFillCaretDownFill /></Flex>
               <Flex display={{base: 'none', lg: 'inline-block'}}><BsFillCaretRightFill /></Flex>
               </HStack>
+              } 
             </Box>
         </Flex>
         <Flex w={{base: '100%', lg: '30%'}}>
-          <motion.div whileTap={{scale: 1.2}} whileHover={{scale: 0.95}} className={memojiStyle.borderCircle}>
+          <motion.div whileTap={{scale: 1.2}} whileHover={{scale: 0.95}} className={memojiStyle.borderCircle} onClick={()=>setIsFistBumped(true)}>
             <Image priority src='/Images/homepage.png' alt='hello' height='1000' width='1000'  className={memojiStyle.borderCircle}/>
           </motion.div>
         </Flex>
