9cebb6f38dcba5823d2ca67ba859b7d13c8d8186
b07502089 <a6140000@gmail.com>
2022-01-28 22:37:34 +0800

    finish homepage opening (including RWD)

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import PageMotionContainer from '../components/PageMotionContainer'
 import { Flex, Heading, Box, useColorModeValue } from '@chakra-ui/react'
+import Image from 'next/image'
 
 export default function Home(props) {
   const normalFontColor = useColorModeValue('#000', '#fff')
+  const themeColor = useColorModeValue('orange.600', 'purple.300')
 
   return (
     <PageMotionContainer>
-      <Flex h='90vh' justifyContent="center" grow="1" flexDirection="row" alignItems="center"> 
-          <Heading as="h1" size="4xl" fontWeight="bold" color={normalFontColor} mb={4}>Hello, I am Frank.</Heading>
+      <Flex h='90vh' w='70vw' justifyContent="center" grow="1" flexDirection={{base: 'column', md: 'row'}} alignItems="center"> 
+        <Flex flexDirection='column' w={{base: '100%', md: '65%'}}>
+            <Heading as="h1" size="4xl" fontWeight="bold" color={themeColor} mb={4}>Hello, I am Frank.</Heading>
+            <Heading as="h2" size="xl" fontWeight="bold" color={normalFontColor} mb={4}>A web developer in Taiwan.</Heading>
+            <Heading as="h2" size="lg" fontWeight="bold" color={normalFontColor} mb={4}>Welcome to my website !</Heading>
+        </Flex>
+        <Flex w={{base: '100%', md: '35%'}}>
+            <Image src='/images/homepage.png' alt='hello' height='1000' width='1000' />
+        </Flex>
       </Flex>
     </PageMotionContainer>
   )
