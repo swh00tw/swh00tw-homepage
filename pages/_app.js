127caccda49711e8c44e1a46155721b2fe87d3f6
b07502089 <a6140000@gmail.com>
2022-01-29 17:49:36 +0800

    add basic redux files

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,20 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../layout/theme'
 import Layout from '../layout/layout'
 import { AnimatePresence } from 'framer-motion'
+import { Provider } from "react-redux";
+import store from '../store/'
 
 function MyApp({ Component, pageProps, router }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <AnimatePresence exitBeforeEnter initial={true}>
-          <Component {...pageProps} key={router.route}/>
-        </AnimatePresence>
-      </Layout>
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <AnimatePresence exitBeforeEnter initial={true}>
+            <Component {...pageProps} key={router.route}/>
+          </AnimatePresence>
+        </Layout>
+      </ChakraProvider>
+    </Provider>
   )
 }
 
