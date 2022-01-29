07a9203c9846edf7f5383a7d0e49a1c1e147e518
b07502089 <a6140000@gmail.com>
2022-01-29 15:11:30 +0800

    ad chatbox animation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import PageMotionContainer from '../components/PageMotionContainer'
 import { Flex, Heading, Box, useColorModeValue, Text, HStack } from '@chakra-ui/react'
 import Image from 'next/image'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import memojiStyle from '../styles/memoji.module.css'
 import { BsFillCaretRightFill, BsFillCaretDownFill } from "react-icons/bs";
 import {useState, useEffect} from 'react'
@@ -12,6 +12,7 @@ export default function Home(props) {
   const normalFontColor = useColorModeValue('#000', '#fff')
   const themeColor = useColorModeValue('orange.600', 'purple.300')
   const bgColor = useColorModeValue('gray.100', 'black')
+  const chatBoxColor = useColorModeValue('gray.300','whiteAlpha.400')
 
   useEffect(() => {
     console.log(isFistBumped);
@@ -20,24 +21,32 @@ export default function Home(props) {
   return (
     <PageMotionContainer>
       <Box bg={bgColor} h={{base: '8vh', md: '8vh'}} display={{base: 'block',lg: 'none'}}/>
-      <Flex  w='70vw' justifyContent="center" grow="1" flexDirection={{base: 'column', lg: 'row'}} alignItems="center"> 
+      <Flex py={{lg: '8vh'}} w='70vw' justifyContent="center" grow="1" flexDirection={{base: 'column', lg: 'row'}} alignItems="center"> 
         <Flex flexDirection='column' w={{base: '100%', lg: '65%'}}>
             <Heading fontSize={['4xl', '5xl']} fontWeight="bold" color={themeColor} mb={4}>Hello, I am Frank.</Heading>
             <Heading fontSize="xl" fontWeight="bold" color={normalFontColor} mb={1}>A web developer in Taiwan.</Heading>
             <Heading fontSize="xl" fontWeight="bold" color={normalFontColor}>Welcome to my website !</Heading>
-            <Box fontFamily='mono' bg={useColorModeValue('gray.300','whiteAlpha.400')} borderRadius='lg' h={{base: '14', md: '7'}} py={1} px={5} mx='auto' mt={5} textAlign="center">
-              {isFistBumped? 
-              <HStack>
-              <Text>Nice 2 meet u! Scroll down to see more.</Text>
-              </HStack>
+            <AnimatePresence exitBeforeEnter>
+              {!isFistBumped?
+              <motion.div key="NotFistBumpedYet" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 0.75}} style={{margin: "auto"}}>
+                <Box fontFamily='mono' bg={chatBoxColor} borderRadius='lg' h={{base: '14', md: '7'}} py={1} px={5} mx='auto' mt={5} textAlign="center">
+                  <HStack>
+                  <Text>Click me to do a fist bump. </Text>
+                  <Flex display={{base: 'inline-block', lg: 'none'}}><BsFillCaretDownFill /></Flex>
+                  <Flex display={{base: 'none', lg: 'inline-block'}}><BsFillCaretRightFill /></Flex>
+                  </HStack> 
+                </Box>
+              </motion.div>
               :
-              <HStack>
-              <Text>Click me to do a fist bump. </Text>
-              <Flex display={{base: 'inline-block', lg: 'none'}}><BsFillCaretDownFill /></Flex>
-              <Flex display={{base: 'none', lg: 'inline-block'}}><BsFillCaretRightFill /></Flex>
-              </HStack>
-              } 
-            </Box>
+              <motion.div key="AlreadfyFistBumped" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 0.75}} style={{margin: "auto"}}>
+                <Box fontFamily='mono' bg={chatBoxColor} borderRadius='lg' h={{base: '14', md: '7'}} py={1} px={5} mx='auto' mt={5} textAlign="center">
+                  <HStack>
+                  <Text>Nice 2 meet u! Scroll down to see more. </Text>
+                  </HStack> 
+                </Box>
+              </motion.div>
+              }
+            </AnimatePresence>
         </Flex>
         <Flex w={{base: '100%', lg: '30%'}}>
           <motion.div whileTap={{scale: 1.2}} whileHover={{scale: 0.95}} className={memojiStyle.borderCircle} onClick={()=>setIsFistBumped(true)}>
