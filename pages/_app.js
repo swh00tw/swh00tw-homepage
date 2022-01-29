fa8ff3571494a6540ee2222485a1c86c5b78edfb
b07502089 <a6140000@gmail.com>
2022-01-29 14:48:28 +0800

    add framer motion on change page

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../layout/theme'
 import Layout from '../layout/layout'
+import { AnimatePresence } from 'framer-motion'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
       <Layout>
-        <Component {...pageProps} />
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Component {...pageProps} key={router.route}/>
+        </AnimatePresence>
       </Layout>
     </ChakraProvider>
   )
