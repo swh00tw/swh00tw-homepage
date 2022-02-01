5d53f735e595599370e5a22f386b434da2b950be
b07502089 <a6140000@gmail.com>
2022-02-01 16:43:13 +0800

    fix bug

--- components/PostCard.js
+++ components/PostCard.js
@@ -15,7 +15,7 @@ function PostCard ({post}){
     const postDate = post.pubDate.split(" ")[0];
 
     return (
-        <Box px={3} py={10} borderRadius='3xl' bg={BoxColor} w={{base: '100%', md: '80%'}}>
+        <>
             <Flex w={{base: '80%', md: '90%'}} borderRadius='xl' justify='center' flexDirection={{base: 'column', md: 'row'}}>
                 <Flex w={{base: '100%', md: '70%'}} flexDirection='column' justify='center' >
                     <Heading fontSize={{base: 'sm', md: 'md'}} align='start' isTruncated>{post.title}</Heading>
@@ -24,11 +24,11 @@ function PostCard ({post}){
                     </Flex>
                 </Flex>
                 <Flex w={{base: '100%', md: '30%'}} justify={{base: 'center', md: 'end'}} py={4}>
-                    <Button size='lg' onClick={()=>{window.open(post.link,"_blank")}} w={{base: '100%', md: '60%'}}>Read</Button>
+                    <Button size='lg' onClick={()=>{window.open(post.link,"_blank")}} w={{base: '100%', md: '60%'}} _focus={{border: 'none'}}>Read</Button>
                 </Flex>
             </Flex>
-            <Divider w='90%' mt={5}/>
-        </Box>
+            <Divider w='90%' mt={3} mb={5}/>
+        </>
     )
 }
 
--- utils/getMediumPosts.js
+++ utils/getMediumPosts.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 // ref: https://stackoverflow.com/a/39646796/17420003
 // rss-to-json-converter ref: https://rss2json.com/#rss_url=https%3A%2F%2Fgithub.com%2Flaravel%2Flaravel%2Freleases.atom
-const medium_instance = axios.create({baseURL: `https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40swh00tw`});
+const medium_instance = axios.create({baseURL: `https://api.rss2json.com/v1`});
 
 const getMediumPosts = async () => {
     try {
-        let res = await medium_instance.get(`/`);
+        let res = await medium_instance.get(`/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40swh00tw`);
         // console.log('res: ',res.data.items);
         return res.data.items;
     } catch (e){
