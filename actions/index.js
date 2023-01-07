357c6771c33d9c0772a139254708c0b80e5b151f
swh00tw <a6140000@gmail.com>
2023-01-07 21:45:18 +0800

    delete package

--- actions/index.js
+++
@@ -1,6 +0,0 @@
-import { FIST_BUMP, FIST_BUMP_ANIMATION } from "../constants/action-types";
-
-const doFistBump = () => ({type: FIST_BUMP})
-const doneFistBumpAnimation = () => ({type: FIST_BUMP_ANIMATION})
-
-export { doFistBump, doneFistBumpAnimation }
--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,22 @@
-import { ColorModeScript } from '@chakra-ui/react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import theme from '../layout/theme'
+import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import theme from "../layout/theme";
 
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-            <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           {/* 👇 Here's the script */}
@@ -18,6 +25,6 @@ export default class Document extends NextDocument {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
