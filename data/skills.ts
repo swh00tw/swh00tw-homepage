85f6a41e54002f5823a7caf35d1fdb9ec4def3b9
swh00tw <a6140000@gmail.com>
2023-01-07 22:04:01 +0800

    to ts

---
+++ data/skills.ts
@@ -0,0 +1,130 @@
+// level: [Beginner, Intermediate, Experienced]
+// ts: timestamp since start learning: https://www.epochconverter.com/
+
+interface SkillInfo {
+  name: string;
+  ref: string;
+  level: "Beginner" | "Intermediate" | "Experienced";
+  description: string;
+  ts: number;
+}
+
+const mySkills: SkillInfo[] = [
+  {
+    name: "ReactJS",
+    ref: "https://reactjs.org/",
+    level: "Experienced",
+    description: "A JavaScript library for building user interfaces.",
+    ts: 1630425600,
+  },
+  {
+    name: "Redux",
+    ref: "https://redux.js.org/",
+    level: "Intermediate",
+    description: "A Predictable State Container for JS Apps.",
+    ts: 1630425600,
+  },
+  {
+    name: "Javascript",
+    ref: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    level: "Experienced",
+    description: "A programming language frequently used for web development.",
+    ts: 1619798400,
+  },
+  {
+    name: "NodeJS",
+    ref: "https://nodejs.org/en/",
+    level: "Intermediate",
+    description: "A JavaScript runtime built on Chrome's V8 JavaScript engine.",
+    ts: 1619798400,
+  },
+  {
+    name: "ExpressJS",
+    ref: "https://expressjs.com/",
+    level: "Intermediate",
+    description: "A minimal and flexible Node.js web application framework.",
+    ts: 1619798400,
+  },
+  {
+    name: "MongoDB",
+    ref: "https://www.mongodb.com/",
+    level: "Intermediate",
+    description: "An open-source, document-oriented database.",
+    ts: 1619798400,
+  },
+  {
+    name: "Mongoose",
+    ref: "https://mongoosejs.com/",
+    level: "Intermediate",
+    description:
+      "A MongoDB object modeling tool designed to work in an asynchronous environment.",
+    ts: 1619798400,
+  },
+  {
+    name: "Chakra UI",
+    ref: "https://chakra-ui.com/",
+    level: "Experienced",
+    description: "A React UI library for building web applications.",
+    ts: 1619798400,
+  },
+  {
+    name: "NextJS",
+    ref: "https://nextjs.org/",
+    level: "Beginner",
+    description:
+      "A minimalistic framework for server-rendered React applications.",
+    ts: 1640966400,
+  },
+  {
+    name: "GraphQL",
+    ref: "https://graphql.org/",
+    level: "Beginner",
+    description: "A query language for your API.",
+    ts: 1638288000,
+  },
+  {
+    name: "Apollo",
+    ref: "https://www.apollographql.com/",
+    level: "Beginner",
+    description: "A GraphQL client for React.",
+    ts: 1638288000,
+  },
+  {
+    name: "Git",
+    ref: "https://git-scm.com/",
+    level: "Experienced",
+    description:
+      "A version control system for tracking changes in source code.",
+    ts: 1598889600,
+  },
+  {
+    name: "Axios",
+    ref: "https://axios-http.com/docs/intro",
+    level: "Intermediate",
+    description: "A Promise based HTTP client for the browser and node.js.",
+    ts: 1630425600,
+  },
+  {
+    name: "Python",
+    ref: "https://www.python.org/",
+    level: "Intermediate",
+    description: "A general-purpose, high-level programming language.",
+    ts: 1567267200,
+  },
+  {
+    name: "Vercel",
+    ref: "https://vercel.com/",
+    level: "Beginner",
+    description: "A hosting platform for static websites.",
+    ts: 1640966400,
+  },
+  {
+    name: "Framer Motion",
+    ref: "https://framer.com/motion/",
+    level: "Beginner",
+    description: "A library for creating animations in React.",
+    ts: 1640966400,
+  },
+];
+
+export default mySkills;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../layout/theme";
 import Layout from "../layout/layout";
 import { AnimatePresence } from "framer-motion";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps, router }) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Layout>
