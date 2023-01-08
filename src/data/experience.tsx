2329027b5014ad1c338f0bc54546b6520e71598b
swh00tw <a6140000@gmail.com>
2023-01-09 01:35:09 +0800

    finish experience section

--- src/data/experience.tsx
+++ src/data/experience.tsx
@@ -1,18 +1,19 @@
-import React from "react";
-
 export interface BaseExperienceInfo {
   startTs: number; // ms
   endTs: number | null; // ms
   type: "Work" | "Project";
   roles: string[];
   location: string | null;
-  description: React.ReactNode;
+  description: string;
+  logoImagePath: string;
   imagePath: string;
   websiteUrl?: string;
+  achievements?: { emoji: string; text: string }[];
 }
 export interface WorkExperienceInfo extends BaseExperienceInfo {
   type: "Work";
   organization: string;
+  team?: string;
   linkedinUrl?: string;
 }
 export interface ProjectExperienceInfo extends BaseExperienceInfo {
@@ -39,11 +40,13 @@ export const experienceSource: ExperienceInfo[] = [
     startTs: 1659225634000,
     endTs: 1673179196000,
     organization: "National Taiwan University, Office of Academic Affairs",
+    team: "NTUCourse Neo Team",
     roles: ["Frontend Engineer", "Frontend Lead"],
     location: "Taipei, Taiwan",
     description:
-      "Developed the brand new course selection system and course information website for NTU",
-    imagePath: "/ntu.jpeg",
+      "The brand new course selection system and course information website",
+    logoImagePath: "/ntu.jpeg",
+    imagePath: "/ntu_nol_neo.png",
   },
   {
     type: "Work",
@@ -53,9 +56,10 @@ export const experienceSource: ExperienceInfo[] = [
     roles: ["Software Engineer Intern"],
     location: "Taipei, Taiwan",
     description:
-      "Developed the workout-tracking app which aims to provide an intuitive application for tracking workout performance and aiding coaches to train CrossFit athletes",
-    imagePath: "/kinetik.png",
+      "The workout-tracking app which aims to provide an intuitive application for tracking performance and aiding coaches to train CrossFit athletes",
+    logoImagePath: "/kinetik.png",
     linkedinUrl: "https://www.linkedin.com/company/kinetikathlete/",
+    imagePath: "/wodsquad.png",
   },
   {
     type: "Project",
@@ -66,10 +70,11 @@ export const experienceSource: ExperienceInfo[] = [
     location: "Taipei, Taiwan",
     description:
       "A redesigned course-planning web service for students at National Taiwan University.",
-    imagePath: "/ncn_logo.png",
+    logoImagePath: "/ncn_logo.png",
     websiteUrl: "https://ncn.pages.dev",
     techStacks: ["React", "Next.js", "Typescript", "PostgreSQL", "Node.js"],
     githubLink: "https://github.com/NTUCourse-Neo",
+    imagePath: "/project-ntucourse-neo.png",
   },
   {
     type: "Project",
@@ -80,8 +85,9 @@ export const experienceSource: ExperienceInfo[] = [
     location: "Taipei, Taiwan",
     description:
       "A data structure visualization tool for beginners in Data Structure",
-    imagePath: "/ds_tutor.png",
+    logoImagePath: "/ds_tutor.png",
     techStacks: ["Javascript", "HTML/Canvas", "TweenJS", "EaselJS"],
     githubLink: "https://github.com/swh00tw/DS_Tutor",
+    imagePath: "/project-ds-tutor.png",
   },
 ];
