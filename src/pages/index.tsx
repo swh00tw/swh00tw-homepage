889e7f517d06b3955b7dbf2402e3a3ae1c0562aa
swh00tw <a6140000@gmail.com>
2023-01-08 16:16:29 +0800

    extract component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -135,107 +135,113 @@ function CustomButton(props: {
   );
 }
 
+function WelcomeSection() {
+  return (
+    <Flex
+      w="80%"
+      minH="90vh"
+      mx="auto"
+      pt="10%"
+      alignItems={"start"}
+      justifyContent="space-between"
+    >
+      <Stack
+        sx={{
+          fontSize: "2rem",
+          fontWeight: 500,
+        }}
+        maxW="60%"
+        pt={"7%"}
+      >
+        <Flex
+          alignItems={"center"}
+          sx={{
+            fontSize: "3rem",
+            fontWeight: 700,
+          }}
+        >
+          Shu-Wei Hsu
+          <Text mx="2" fontSize="2.5rem">
+            🇹🇼
+          </Text>
+        </Flex>
+        <Flex
+          sx={{
+            fontWeight: 600,
+          }}
+          wrap="wrap"
+        >
+          <AnimatedColorText endColor="#4FD1C5" duration={10}>
+            {"Engineering Student"}
+          </AnimatedColorText>
+          <Text mx={2}>/</Text>
+          <AnimatedColorText endColor="#F687B3" isReversed duration={10}>
+            {"Web Developer"}
+          </AnimatedColorText>
+        </Flex>
+        <Stack
+          pt={4}
+          w="90%"
+          sx={{
+            color: "#ffffff90",
+            fontSize: "1.1rem",
+          }}
+        >
+          <Text>
+            I am a Taiwanese full-stack web engineer who is experienced in
+            React, Next.js, and Node.js.
+          </Text>
+          <Text>
+            Also an enthusiast of frontend technologies and UI/UX design.
+          </Text>
+        </Stack>
+        <HStack>
+          <CustomButton bg="linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(29,253,237,1) 51%, rgba(69,252,196,1) 100%)">
+            <a href="/Frank_resume.pdf" download="Frank_resume.pdf">
+              <HStack>
+                <FaDownload color={"white"} />
+                <Text color={"white"}>Resume</Text>
+              </HStack>
+            </a>
+          </CustomButton>
+          <CustomButton
+            link="https://github.com/swh00tw"
+            bg="linear-gradient(90deg, rgba(180,118,58,1) 23%, rgba(253,29,239,0.8954175420168067) 59%, rgba(252,69,105,1) 87%)"
+          >
+            <HStack>
+              <FaGithub color={"white"} />
+              <Text color={"white"}>swh00tw</Text>
+            </HStack>
+          </CustomButton>
+        </HStack>
+      </Stack>
+      <motion.div
+        whileHover={{
+          scale: 1.05,
+        }}
+        whileTap={{
+          scale: 0.95,
+        }}
+      >
+        <Image
+          quality="100"
+          priority
+          src="/Images/stopit.png"
+          alt="hello"
+          height="450"
+          width="450"
+          className={memojiStyle.borderCircle}
+        />
+      </motion.div>
+    </Flex>
+  );
+}
+
 export default function Home() {
   return (
     <PageHeaderWrapper>
       <PageMotionContainer duration={0.75}>
-        <Flex
-          w="80%"
-          minH="90vh"
-          mx="auto"
-          pt="10%"
-          alignItems={"start"}
-          justifyContent="space-between"
-        >
-          <Stack
-            sx={{
-              fontSize: "2rem",
-              fontWeight: 500,
-            }}
-            maxW="60%"
-            pt={"7%"}
-          >
-            <Flex
-              alignItems={"center"}
-              sx={{
-                fontSize: "3rem",
-                fontWeight: 700,
-              }}
-            >
-              Shu-Wei Hsu
-              <Text mx="2" fontSize="2.5rem">
-                🇹🇼
-              </Text>
-            </Flex>
-            <Flex
-              sx={{
-                fontWeight: 600,
-              }}
-              wrap="wrap"
-            >
-              <AnimatedColorText endColor="#4FD1C5" duration={10}>
-                {"Engineering Student"}
-              </AnimatedColorText>
-              <Text mx={2}>/</Text>
-              <AnimatedColorText endColor="#F687B3" isReversed duration={10}>
-                {"Web Developer"}
-              </AnimatedColorText>
-            </Flex>
-            <Stack
-              pt={4}
-              w="90%"
-              sx={{
-                color: "#ffffff90",
-                fontSize: "1.1rem",
-              }}
-            >
-              <Text>
-                I am a Taiwanese full-stack web engineer who is experienced in
-                React, Next.js, and Node.js.
-              </Text>
-              <Text>
-                Also an enthusiast of frontend technologies and UI/UX design.
-              </Text>
-            </Stack>
-            <HStack>
-              <CustomButton bg="linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(29,253,237,1) 51%, rgba(69,252,196,1) 100%)">
-                <a href="/Frank_resume.pdf" download="Frank_resume.pdf">
-                  <HStack>
-                    <FaDownload color={"white"} />
-                    <Text color={"white"}>Resume</Text>
-                  </HStack>
-                </a>
-              </CustomButton>
-              <CustomButton
-                link="https://github.com/swh00tw"
-                bg="linear-gradient(90deg, rgba(180,118,58,1) 23%, rgba(253,29,239,0.8954175420168067) 59%, rgba(252,69,105,1) 87%)"
-              >
-                <HStack>
-                  <FaGithub color={"white"} />
-                  <Text color={"white"}>swh00tw</Text>
-                </HStack>
-              </CustomButton>
-            </HStack>
-          </Stack>
-          <motion.div
-            whileHover={{
-              scale: 1.05,
-            }}
-            whileTap={{
-              scale: 0.95,
-            }}
-          >
-            <Image
-              quality="100"
-              priority
-              src="/Images/stopit.png"
-              alt="hello"
-              height="450"
-              width="450"
-              className={memojiStyle.borderCircle}
-            />
-          </motion.div>
-        </Flex>
+        <WelcomeSection />
         <Box h="100vh" w="80%" mx="auto">
           placeholder section 2
         </Box>
