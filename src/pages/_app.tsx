ec9ab817dc7216482635dc81f1f1271d8f855b98
swh00tw <a6140000@gmail.com>
2023-01-11 01:55:29 +0800

    fix bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { AppProps } from "next/app";
 import HeaderBar from "@/components/HeaderBar";
 import Footer from "@/components/Footer";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   const [isScrolled, setIsSrolled] = useState(false);
@@ -13,6 +14,13 @@ function MyApp({ Component, pageProps, router }: AppProps) {
       setIsSrolled((window.scrollY ?? 0) > 0);
     });
   }, []);
+
+  const { pathname } = useRouter();
+  useEffect(() => {
+    // some browsers (like safari) may require a timeout to delay calling this
+    // function after a page has loaded; otherwise, it may not update the position
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <ChakraProvider theme={theme}>
