afe3904c80c4dd8e2a5b91a930cfd379b8216455
swh00tw <a6140000@gmail.com>
2023-05-27 23:19:37 +0800

    refactor code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import clsx from "clsx";
-import PageWrapper from "@/app/components/PageWrapper";
+import DesktopPages from "@/app/components/DesktopPages";
 import { PageContextProvider } from "@/app/components/PageProvider";
 import Footer from "@/app/components/Footer";
 import Headerbar from "@/app/components/HeaderBar";
-import { pages as mobilePages } from "@/app/components/MobilePages";
+import MobilePages from "@/app/components/MobilePages";
 
 export default function Home() {
   console.log(typeof window !== "undefined" ? "client" : "server");
@@ -11,18 +11,8 @@ export default function Home() {
     <PageContextProvider>
       <div className={clsx("lg:h-[calc(100svh+4000px)]", "relative")}>
         <Headerbar />
-        <PageWrapper />
-        <div
-          className={clsx(
-            "flex",
-            "lg:hidden",
-            "flex-col",
-            "items-center",
-            "justify-center"
-          )}
-        >
-          {...mobilePages}
-        </div>
+        <DesktopPages />
+        <MobilePages />
         <Footer />
       </div>
     </PageContextProvider>
