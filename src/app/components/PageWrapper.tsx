2b6aa537a977ba97cf26053d3e2544858cc51033
swh00tw <a6140000@gmail.com>
2023-05-27 23:16:23 +0800

    refactor code

--- src/app/components/PageWrapper.tsx
+++ src/app/components/PageWrapper.tsx
@@ -20,44 +20,28 @@ export const pages = [
   { component: <ContactPage key="contact" />, tag: "Contact" },
 ];
 
-export default function PageWrapper(props: {
-  readonly children: React.ReactNode;
-}) {
-  const { children } = props;
+export default function PageWrapper() {
   const { pageIndex } = usePageContext();
 
   return (
-    <React.Fragment>
-      <div
-        className={clsx(
-          "hidden",
-          "lg:flex",
-          "fixed",
-          "top-0",
-          "w-full",
-          "h-screen",
-          "items-center",
-          "justify-center",
-          "text-black"
-        )}
-      >
-        <AnimatePresence mode="wait">
-          <MotionWrapper key={pages[pageIndex].tag}>
-            {pages[pageIndex].component}
-          </MotionWrapper>
-        </AnimatePresence>
-      </div>
-      <div
-        className={clsx(
-          "flex",
-          "lg:hidden",
-          "flex-col",
-          "items-center",
-          "justify-center"
-        )}
-      >
-        {children}
-      </div>
-    </React.Fragment>
+    <div
+      className={clsx(
+        "hidden",
+        "lg:flex",
+        "fixed",
+        "top-0",
+        "w-full",
+        "h-screen",
+        "items-center",
+        "justify-center",
+        "text-black"
+      )}
+    >
+      <AnimatePresence mode="wait">
+        <MotionWrapper key={pages[pageIndex].tag}>
+          {pages[pageIndex].component}
+        </MotionWrapper>
+      </AnimatePresence>
+    </div>
   );
 }
