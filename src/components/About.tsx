30ad836ae0814ac8d46039acde44b571079ea659
swh00tw <a6140000@gmail.com>
2023-05-27 21:25:39 +0800

    refactor css code

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -42,91 +42,93 @@ export default function AboutPage() {
           duration: 0.5,
         },
       }}
-      className="w-[80%] lg:w-[60%] lg:h-[100lvh] flex justify-center items-center py-[8vh]"
     >
-      <div
-        className={clsx(
-          "flex",
-          "flex-col",
-          "lg:flex-row",
-          "w-full",
-          "lg:justify-center",
-          "items-center"
-        )}
-      >
-        <div className={clsx("w-full", "lg:w-[55%]", "lg:pl-[5%]")}>
-          <h1 className={clsx("text-[30px]", "font-semibold")}>
-            Hi, I am <span className="text-primary">Frank</span>
-          </h1>
-          <div className={clsx("my-[36px]")}>
-            <p>
-              I'm a Taiwanese Full Stack Developer with a strong interest in web
-              development and UI/UX design.
-            </p>
-            <div className="h-3" />
-            <p>
-              Currently, I'm studying Connective Media at{" "}
-              <Link href="https://tech.cornell.edu/" target="_blank">
-                <span className="text-[#AA162C] hover:underline">
-                  Cornell Tech
-                </span>
-              </Link>{" "}
-              in New York City. Feel free to explore my website and check out my
-              projects and skills.
-            </p>
+      <div className="lg:h-[100lvh] flex justify-center items-center py-[8vh]">
+        <div
+          className={clsx(
+            "w-[80%]",
+            "lg:w-[60%]",
+            "flex",
+            "flex-col",
+            "lg:flex-row",
+            "lg:justify-center",
+            "items-center"
+          )}
+        >
+          <div className={clsx("w-full", "lg:w-[55%]", "lg:pl-[5%]")}>
+            <h1 className={clsx("text-[30px]", "font-semibold")}>
+              Hi, I am <span className="text-primary">Frank</span>
+            </h1>
+            <div className={clsx("my-[36px]")}>
+              <p>
+                I'm a Taiwanese Full Stack Developer with a strong interest in
+                web development and UI/UX design.
+              </p>
+              <div className="h-3" />
+              <p>
+                Currently, I'm studying Connective Media at{" "}
+                <Link href="https://tech.cornell.edu/" target="_blank">
+                  <span className="text-[#AA162C] hover:underline">
+                    Cornell Tech
+                  </span>
+                </Link>{" "}
+                in New York City. Feel free to explore my website and check out
+                my projects and skills.
+              </p>
+            </div>
+            <div
+              className={clsx(
+                "flex",
+                "flex-wrap",
+                "justify-start",
+                "gap-x-2",
+                "w-full"
+              )}
+            >
+              <EducationRecordCard
+                schoolName="National Taiwan University"
+                period="Sep, 2018 - Jan, 2023"
+              >
+                <Image
+                  src="/ntu_logo.png"
+                  width={50}
+                  height={50}
+                  className="aspect-square"
+                  alt="NTU Logo"
+                />
+              </EducationRecordCard>{" "}
+              <EducationRecordCard
+                schoolName="Cornell Tech"
+                period="Aug, 2023 - May, 2025"
+              >
+                <Image
+                  src="/cornelltech_logo.png"
+                  width={50}
+                  height={50}
+                  className="aspect-square"
+                  alt="CT Logo"
+                />
+              </EducationRecordCard>
+            </div>
           </div>
           <div
             className={clsx(
+              "hidden",
+              "lg:block",
+              "lg:w-[45%]",
               "flex",
-              "flex-wrap",
-              "justify-start",
-              "gap-x-2",
-              "w-full"
+              "justify-center"
             )}
           >
-            <EducationRecordCard
-              schoolName="National Taiwan University"
-              period="Sep, 2018 - Jan, 2023"
-            >
-              <Image
-                src="/ntu_logo.png"
-                width={50}
-                height={50}
-                className="aspect-square"
-                alt="NTU Logo"
-              />
-            </EducationRecordCard>{" "}
-            <EducationRecordCard
-              schoolName="Cornell Tech"
-              period="Aug, 2023 - May, 2025"
-            >
-              <Image
-                src="/cornelltech_logo.png"
-                width={50}
-                height={50}
-                className="aspect-square"
-                alt="CT Logo"
-              />
-            </EducationRecordCard>
+            <Image
+              src="/About.png"
+              width={300}
+              height={300}
+              alt="About me memoji"
+              style={{ margin: "0 auto" }}
+              priority
+            />
           </div>
-        </div>
-        <div
-          className={clsx(
-            "hidden",
-            "lg:block",
-            "lg:w-[45%]",
-            "flex",
-            "justify-center"
-          )}
-        >
-          <Image
-            src="/About.png"
-            width={300}
-            height={300}
-            alt="About me memoji"
-            style={{ margin: "0 auto" }}
-            priority
-          />
         </div>
       </div>
     </motion.div>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -52,75 +52,91 @@ export default function ContactPage() {
           duration: 0.5,
         },
       }}
-      className="w-[100%] lg:w-[60%] h-[100lvh] flex justify-start pt-[18vh] md:pt-[25vh] lg:pt-0 overflow-hidden relative"
     >
       <div
         className={clsx(
+          "w-screen",
+          "h-[100lvh]",
           "flex",
-          "flex-col",
-          "lg:flex-row",
-          "w-full",
-          "lg:justify-between",
-          "items-center"
+          "justify-center",
+          "pt-[18vh]",
+          "md:pt-[25vh]",
+          "lg:pt-0",
+          "overflow-hidden",
+          "relative"
         )}
       >
         <div
           className={clsx(
-            "mb-[120px]",
-            "lg:mb-0",
-            "w-[80%]",
-            "lg:w-[65%]",
-            "lg:pl-[5%]"
-          )}
-        >
-          <div className={clsx("text-[30px]", "font-semibold")}>Contact me</div>
-          <p className="py-7">
-            Thanks for visiting! Hope you find something here interesting.
-            <br /> Feel free to reach out to me!
-          </p>
-          <div className={clsx("flex", "flex-col")}>
-            <ContactItem
-              platform="Email"
-              href="mailto:a6140000@gmail.com"
-              content="a6140000@gmail.com"
-            />
-            <ContactItem
-              platform="Github"
-              href="https://github.com/swh00tw"
-              content="github.com/swh00tw"
-            />
-            <ContactItem
-              platform="LinkedIn"
-              href="https://linkedin.com/in/swh00tw"
-              content="linkedin.com/in/swh00tw"
-            />
-          </div>
-        </div>
-        <div
-          className={clsx(
             "flex",
-            "justify-center",
-            "absolute",
-            "lg:static",
-            "w-[150%]",
-            "lg:w-[30%]",
-            "h-[500px]",
-            "lg:h-auto",
-            "bottom-[-220px]",
-            "md:bottom-[-150px]",
-            "bg-[#9FAFDF50]",
-            "lg:bg-transparent",
-            "rounded-gestureSvg"
+            "flex-col",
+            "lg:flex-row",
+            "w-[100%]",
+            "lg:w-[60%]",
+            "lg:justify-between",
+            "items-center"
           )}
         >
-          <Image
-            src="/Contact.png"
-            width={200}
-            height={253}
-            alt="contact me"
-            className={clsx("hidden", "lg:block")}
-          />
-          <GestureImage className="lg:hidden w-[275px] h-[396px]" />
+          <div
+            className={clsx(
+              "mb-[120px]",
+              "lg:mb-0",
+              "w-[80%]",
+              "lg:w-[65%]",
+              "lg:pl-[5%]"
+            )}
+          >
+            <div className={clsx("text-[30px]", "font-semibold")}>
+              Contact me
+            </div>
+            <p className="py-7">
+              Thanks for visiting! Hope you find something here interesting.
+              <br /> Feel free to reach out to me!
+            </p>
+            <div className={clsx("flex", "flex-col")}>
+              <ContactItem
+                platform="Email"
+                href="mailto:a6140000@gmail.com"
+                content="a6140000@gmail.com"
+              />
+              <ContactItem
+                platform="Github"
+                href="https://github.com/swh00tw"
+                content="github.com/swh00tw"
+              />
+              <ContactItem
+                platform="LinkedIn"
+                href="https://linkedin.com/in/swh00tw"
+                content="linkedin.com/in/swh00tw"
+              />
+            </div>
+          </div>
+          <div
+            className={clsx(
+              "flex",
+              "justify-center",
+              "absolute",
+              "lg:static",
+              "w-[150%]",
+              "lg:w-[30%]",
+              "h-[500px]",
+              "lg:h-auto",
+              "bottom-[-220px]",
+              "md:bottom-[-150px]",
+              "bg-[#9FAFDF50]",
+              "lg:bg-transparent",
+              "rounded-gestureSvg"
+            )}
+          >
+            <Image
+              src="/Contact.png"
+              width={200}
+              height={253}
+              alt="contact me"
+              className={clsx("hidden", "lg:block")}
+            />
+            <GestureImage className="lg:hidden w-[275px] h-[396px]" />
+          </div>
         </div>
       </div>
     </motion.div>
