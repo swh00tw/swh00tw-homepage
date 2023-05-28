b19e46d07784bcb4e0cdc9bd9aa2d3a6e5513ae3
swh00tw <a6140000@gmail.com>
2023-05-28 17:26:17 +0800

    clear console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Headerbar from "@/app/components/HeaderBar";
 import MobilePages from "@/app/components/MobilePages";
 
 export default function Home() {
-  console.log(typeof window !== "undefined" ? "client" : "server");
   return (
     <PageContextProvider>
       <div className={clsx("lg:h-[calc(100svh+4000px)]", "relative")}>
