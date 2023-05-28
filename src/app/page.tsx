a4412b63bd5a48bfebfb9356bacc80e8646e93ca
swh00tw <a6140000@gmail.com>
2023-05-28 17:32:31 +0800

    disable grainy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ import MobilePages from "@/app/components/MobilePages";
 export default function Home() {
   return (
     <PageContextProvider>
-      <div className={clsx("lg:h-[calc(100svh+4000px)]", "relative")}>
+      <div
+        className={clsx("lg:h-[calc(100svh+4000px)]", "relative", "bg-main")}
+      >
         <Headerbar />
         <DesktopPages />
         <MobilePages />
