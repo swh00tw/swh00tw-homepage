60cdd7d07f06fca5dc3ffc273231ee035fc18e97
swh00tw <a6140000@gmail.com>
2024-05-27 18:45:10 -0400

    chore: refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,10 +82,6 @@ export default function Home() {
     if (typeof window === "undefined") {
       return { width: 0, height: 0 };
     }
-    if (ref) {
-      const { width, height } = ref.getBoundingClientRect();
-      return { width, height };
-    }
     return { width: window.innerWidth, height: window.innerHeight };
   });
   const gridBoxWidth = 18;
