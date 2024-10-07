a091e5230f60cb597c3d842445d231089aed8105
swh00tw <a6140000@gmail.com>
2024-10-06 23:15:13 -0400

    feat: finish post pages

--- src/app/log/[slug]/page.tsx
+++ src/app/log/[slug]/page.tsx
@@ -1,11 +1,50 @@
+import fs from "fs";
+import path from "path";
 import { NewTabWrapper } from "@/app/NewTabWrapper";
+import { postsMarkdownPath } from "@/app/constant";
+import { getPost } from "./getPost";
+import { MDXCustom } from "@/app/mdx/mdx-components";
 
-export default function Page() {
+export async function generateStaticParams() {
+  const files = fs.readdirSync(path.join(postsMarkdownPath), {
+    withFileTypes: true,
+  });
+
+  const paths = files
+    .filter((f) => f.isFile())
+    .map((f) => f.name)
+    .map((filename) => ({
+      slug: filename.endsWith(".mdx")
+        ? filename.replace(".mdx", "")
+        : filename.replace(".md", ""),
+    }));
+
+  return paths;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const blog = getPost({
+    slug: params.slug,
+  });
+
+  return {
+    title: blog.frontMatter.title,
+    description: blog.frontMatter.description,
+  };
+}
+
+export default function Page({ params }: { params: { slug: string } }) {
+  const data = getPost({
+    slug: params.slug,
+  });
+
   return (
     <NewTabWrapper>
-      <div>
-        <h1>Page</h1>
-      </div>
+      <MDXCustom source={data.content} />
     </NewTabWrapper>
   );
 }
--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -33,7 +33,7 @@ export async function generateMetadata({
 
   return {
     title: blog.frontMatter.displayName,
-    description: "Frank Hsu",
+    description: blog.frontMatter.desc || "Frank Hsu - Projects",
   };
 }
 
